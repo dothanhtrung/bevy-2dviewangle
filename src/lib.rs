6dcece17d3bf1aa5653e314932b4505950c0d06c
Trung Do <dothanhtrung@pm.me>
2024-01-30 23:15:34 +0700

    Add overview docs for crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 // Copyright 2024 Trung Do <dothanhtrung@pm.me>
 
+//! Bevy plugin to easier to switch texture base on view angles. Currently, support 8 view angles:
+//!
+//! * front
+//! * back
+//! * left
+//! * right
+//! * front_left
+//! * front_right
+//! * back_left
+//! * back_right
+//!
+//! Examples
+//! --------
+//!
+//! * [2d example](https://gitlab.com/kimtinh/bevy-2dviewangle/-/blob/master/examples/2d.rs)
+//! * [3d example](https://gitlab.com/kimtinh/bevy-2dviewangle/-/blob/master/examples/3d.rs)
+
 use bevy::app::{App, Plugin, Update};
 
 pub use component::*;
